0e3fd60ad6f3c9089771b2a08eb41e21b76a08d0
doooomdoom <kgsyhs0301@naver.com>
2020-03-25 20:19:23 +0900

    카카오 맵 API연동

---
+++ react/hanbang/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import DefaultMap from './Components/DefaultMap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <DefaultMap>
+          
+        </DefaultMap>
+      </div>
+    );
+  } 
+}
+
+export default App;
---
+++ react/hanbang/src/Components/DefaultMap.js
@@ -0,0 +1,27 @@
+/*global kakao*/
+import React from 'react';
+import './DefaultMap.css'
+
+class DefaultMap extends React.Component {
+   
+    constructor(props) {
+        super(props);	
+    }
+
+    componentDidMount(){
+        var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+        var options = { //지도를 생성할 때 필요한 기본 옵션
+            center: new kakao.maps.LatLng(35.157588, 129.058822), //지도의 중심좌표.
+            level: 5 //지도의 레벨(확대, 축소 정도)
+        };
+	    this.map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    }
+
+    render() {
+        return (
+            <div id='map'>
+            </div>
+        )
+    }
+}
+export default DefaultMap;

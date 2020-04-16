ee2361bb76c62b8d4b711e2469ff88fc189722fe
HoYoon-Lee <happylhy0630@naver.com>
2020-04-16 15:09:23 +0900

    Django setting

--- project/nhyc/nhyc/nhyc/settings.py
+++ project/nhyc/nhyc/nhyc/settings.py
@@ -37,7 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    #test
+    'Server.apps.ServerConfig',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

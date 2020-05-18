7e7950ebe3f0a3fc9541b5215b43bd15bdb7045f
HoYoon-Lee <happylhy0630@naver.com>
2020-05-18 10:47:11 +0900

    Django DB 연결1

--- project/nhyc/nhyc/dataProcess/models.py
+++ project/nhyc/nhyc/dataProcess/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Member(models.Model):
-    id = models.CharField(max_length=50)
+    id = models.CharField(max_length=50, primary_key=True)
     password = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
 
@@ -9,32 +9,32 @@ class Member(models.Model):
         return self.id
 
 class Address(models.Model):
-    areaCode = models.CharField(max_length=10)
+    areaCode = models.CharField(max_length=10, primary_key=True)
     si = models.CharField(max_length=50)
     gu = models.CharField(max_length=50)
     dong = models.CharField(max_length=50)
 
 class MemberInfo(models.Model):
-    id = models.ForeignKey('Member', on_delete=models.CASCADE)
+    id = models.ForeignKey('Member', on_delete=models.CASCADE, primary_key=True)
     gender = models.CharField(max_length=1)
-    birth = models.IntegerField(max_length=4)
+    birth = models.CharField(max_length=4)
     money = models.IntegerField()
 
     def __str__(self):
         return self.id
 
 class FrequentPlace(models.Model):
-    id = models.ForeignKey('Member', on_delete=models.CASCADE)
+    id = models.ForeignKey('Member', on_delete=models.CASCADE, primary_key=True)
+    latitude = models.FloatField(primary_key=True)
+    longitude = models.FloatField(primary_key=True)
     placeName = models.CharField(max_length=100)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
     areaCode = models.ForeignKey('Address', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.placeName
 
 class HouseInfo(models.Model):
-    houseNumber = models.IntegerField()
+    houseNumber = models.IntegerField(primary_key=True)
     latitude = models.FloatField()
     longitude = models.FloatField()
     pyeong = models.FloatField()
@@ -45,7 +45,7 @@ class HouseInfo(models.Model):
         return self.houseName
 
 class CostRecord(models.Model):
-    houseNumber = models.ForeignKey('HouseInfo', on_delete=models.CASCADE)
+    houseNumber = models.ForeignKey('HouseInfo', on_delete=models.CASCADE, primary_key=True)
     day = models.DateField()
     rentalFee = models.IntegerField()
     deposit = models.IntegerField()
@@ -54,24 +54,24 @@ class CostRecord(models.Model):
         return [self.day, self.rentalFee, self.deposit]
 
 class CCTV(models.Model):
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    latitude = models.FloatField(primary_key=True)
+    longitude = models.FloatField(primary_key=True)
     areaCode = models.ForeignKey('Address', on_delete=models.CASCADE)
 
     def __str__(self):
         return [self.latitude, self.longitude]
 
 class SecurityLight(models.Model):
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    latitude = models.FloatField(primary_key=True)
+    longitude = models.FloatField(primary_key=True)
     areaCode = models.ForeignKey('Address', on_delete=models.CASCADE)
 
     def __str__(self):
         return [self.latitude, self.longitude]
 
 class PoliceOffice(models.Model):
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    latitude = models.FloatField(primary_key=True)
+    longitude = models.FloatField(primary_key=True)
     policeOfficeName = models.CharField(max_length=50)
     areaCode = models.ForeignKey('Address', on_delete=models.CASCADE)
 
--- project/nhyc/nhyc/nhyc/settings.py
+++ project/nhyc/nhyc/nhyc/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     #test
-    'Server.apps.ServerConfig',
+    'dataProcess'
 ]
 
 REST_FRAMEWORK = {
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'nhyc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hanbangdb',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 

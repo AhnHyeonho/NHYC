30b0db7887dd93ad13499320c92c7e8d6dfaa121
AhnHyeonho <ahndding@gmail.com>
2020-05-18 15:30:17 +0900

    복구작업1

--- project/nhyc/nhyc/dataProcess/admin.py
+++ project/nhyc/nhyc/dataProcess/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
 
-from .models import Member
-from .models import Address
-from .models import MemberInfo
-from .models import FrequentPlace
-from .models import HouseInfo
-from .models import CostRecord
-from .models import CCTV
-from .models import SecurityLight
-from .models import PoliceOffice
+# Register your models here.

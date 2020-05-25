d7b0b9304e529cd811afb857b64593733e903904
doooomdoom <kgsyhs0301@naver.com>
2020-05-26 02:54:46 +0900

    막대그래프 대략적 형태 완성 - CSS 디테일 수정해야함 - API와 연동되도록 데이터 넣는 부분 수정하기 - 툴팁, 하단 항목 이름 및 그래프 타이틀 그래프 객체별로 적용되게 바꾸기

--- react/hanbang/src/Components/App.js
+++ react/hanbang/src/Components/App.js
@@ -6,7 +6,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <BarGraph />
+        <BarGraph name="서울시 면적당 CCTV 설치 현황"/>
+        <BarGraph name="서울시 경찰서 / 파출소 / 지구대 현황"/>
+        <BarGraph name="서울시 가로등 설치 현황"/>
       </div>
     );
   }
--- react/hanbang/src/Components/BarGraph.js
+++ react/hanbang/src/Components/BarGraph.js
@@ -1,40 +1,80 @@
 import React, { Component } from 'react'
-import {Bar} from 'react-chartjs-2';
- 
-const data = {
-	labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-	datasets: [
-	  {
-		label: 'My First dataset',
-		backgroundColor: 'rgba(244,98,58,1)',
-		borderColor: 'rgba(244,98,58,1)',
-		borderWidth: 1,
-		hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-		hoverBorderColor: 'rgba(255,99,132,1)',
-		data: [65, 59, 80, 81, 56, 55, 40]
-	  }
-	]
-  };
-  
+import { Bar } from 'react-chartjs-2';
+import './BarGraph.css'
 
-  class BarGraph extends Component {
+const data = {
+
+	labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August'],
+	datasets: [
+		{
+			label: 'My First dataset',
+			backgroundColor: 'rgba(41,181,189,1)',
+			borderColor: 'rgba(41,181,189,1)',
+			borderWidth: 1,
+			hoverBackgroundColor: 'rgba(41,181,189,0.4)',
+			hoverBorderColor: 'rgba(41,181,189,1)',
+			data: [65, 59, 80, 81, 56, 55, 40, 100]
+		}
+	]
+};
+
+// Bar Graph 옵션값 
+var options = {
+	responsive:false, // true로 바꾸면 화면에 꽉차게끔 그래프가 그려짐 (default = true)
+
+	tooltips: {
+		callbacks: {
+			label: function (tooltipItem) {
+				return "$" + Number(tooltipItem.yLabel) + " and so worth it !";
+			}
+		}
+	},
+
+	title: {
+		display: true,
+		text: 'Ice Cream Truck',
+		position: 'bottom'
+	},
+
+	hover:{
+		onHover: function (e, element) {
+			console.log(e);
+		}
+	},
+
+	scales: {
+		yAxes: [{
+			ticks: {
+				beginAtZero: true
+			}
+		}]
+	},
+
+	maintainAspectRatio: true
+};
+
+
+class BarGraph extends Component {
+
+	componentWillMount() {
+
+    }
+
 	render() {
 		return (
-		  <div>
-			<h2>Bar Example (custom size)</h2>
-			<Bar
-			  data={data}
-			  width={100}
-			  height={50}
-			  options={{
-				maintainAspectRatio: true
-			  }}
-			/>
-		  </div>
+			<div>
+				<h2 className="title"><span className="line"/>  {this.props.name}</h2>
+				<Bar 
+					data={data}
+					width="500vh" // 그래프 전체의 width 
+					height={300} // 그래프 전체의 Height
+					options={options}
+				/>
+			</div>
 		);
-	  }
+	}
 
-  }
+}
 
 
-  export default BarGraph;
+export default BarGraph;

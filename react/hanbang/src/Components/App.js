609ed220a4e4d14e51238ada40aff901f4b9a85b
doooomdoom <kgsyhs0301@naver.com>
2020-05-28 01:39:27 +0900

    states 예제 테스트

--- react/hanbang/src/Components/App.js
+++ react/hanbang/src/Components/App.js
@@ -1,24 +1,16 @@
-import React, {Component} from 'react';
-import BarGraph from './BarGraph';
+import React, { Component } from 'react';
+import BarGraph from './GraphArea';
 import './App.css'
+import GraphArea from './GraphArea';
 
-
-// react-vis
 class App extends Component {
   render() {
 
-    const seoulGu = ['강동구', '노원구', '노원구', '노원구', '노원구', '노원구', '노원구', '노원구' ]
-
     return (
-      <div className="graph-area" >
-        <BarGraph name="서울시 면적당 CCTV 설치 현황" labelData={seoulGu}/>
-        <BarGraph name="서울시 경찰서 / 파출소 / 지구대 현황" labelData={seoulGu}/>
-        <BarGraph name="서울시 가로등 설치 현황" labelData={seoulGu}/>
-      </div>
-    );
+      <GraphArea />
+    )
   }
+
 }
-
-
 
 export default App;

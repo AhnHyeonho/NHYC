d113b8bcd687db68823009f75958826dc7cb8887
doooomdoom <kgsyhs0301@naver.com>
2020-06-02 19:35:06 +0900

    6/1 - 메인화면 css 변경, 추천페이지 컴포넌트 작성중

--- react/jk-react/src/App.js
+++ react/jk-react/src/App.js
@@ -1,24 +1,30 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from './Page/Header';
 import Footer from './Page/Footer';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import {About, MapInfo} from './Page';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { About, MapInfo, Recommand } from './Page';
 
 function App() {
 
 
   return (
     <Router>
-      <div className="App"> 
-        <div className="App-Header"><Header /></div>
-        <div className ="App-Body">
-          <Route exact path ='/' component ={About}/>
-          <Route exact path ='/about' component ={About}/>
-          <Route path ='/map' component={MapInfo}/>
-        </div>
-        {/*<Footer/>*/}
+
+      {/* header */}
+      <div className="App-Header">
+        <Header />
       </div>
+
+      {/* body */}
+      <div className="App-Body">
+        <Route exact path='/' component={About} />
+        <Route exact path='/about' component={About} />
+        <Route path='/map' component={MapInfo} />
+        {/* <Route path='/recommand' component={Recommand} /> */}
+      </div>
+
+      {/* footer */}
     </Router>
   );
 }
--- react/jk-react/src/Page/Header.js
+++ react/jk-react/src/Page/Header.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import './Header.css';
 import logo from '../Img/logo.png'
-class Header extends React.Component{
+class Header extends React.Component {
 
-    render(){
-        return <div className="app-header">
-            <img src={logo} className="header logo"></img>
-            <a href="/about" className="header about">About</a>
-            <a href="/map" className="header map">Map</a>
-            <a href="#" className="header graph">Graph</a>
-            <a href="#" className="header info">Info</a>
+    render() {
+        return (
+
+            <div className="header-block">
+                
+                {/* 로고 */}
+                <div className="header logo">Hanbang</div>
+
+                {/* 버튼 묶음 */}
+                <div className="header buttons">
+                    <a href="/about" className="header item">About</a>
+                    <a href="/map" className="header item">Map</a>
+                    <a href="#" className="header item">Recommand</a>
+                    <a href="#" className="header item">Info</a>
+                </div>
             </div>
+        )
     }
 }
-
-{/*<a style={{'text-decoration': "none" }} href="/about" className="header about">About</a>*/}
-
-
-
 
 export default Header;

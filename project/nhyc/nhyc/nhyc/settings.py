8cc85d82a6b922f3eacfd84e909b1d06d38a6897
HoYoon-Lee <happylhy0630@naver.com>
2020-06-16 22:33:24 +0900

    CORS 관련 세팅 추가

--- project/nhyc/nhyc/nhyc/settings.py
+++ project/nhyc/nhyc/nhyc/settings.py
@@ -78,6 +78,27 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
+
 ROOT_URLCONF = 'nhyc.urls'
 
 TEMPLATES = [
